f8bbc5072cbc5da76a9c7d8148b381ed2384ddc7
ZenoVux <zenovux0702@gmail.com>
2021-10-07 23:59:29 +0700

    zenovux

---
+++ src/real/map/ItemMap.java
@@ -0,0 +1,5 @@
+package real.map;
+
+public class ItemMap {
+    
+}
---
+++ src/real/map/Mob.java
@@ -0,0 +1,22 @@
+package real.map;
+
+public class Mob {
+
+    public int tempId;
+
+    public byte level;
+
+    public int hp;
+
+    public int maxHp;
+
+    public short pointX;
+
+    public short pointY;
+    
+    public byte status;
+
+    public void update() {
+
+    }
+}
